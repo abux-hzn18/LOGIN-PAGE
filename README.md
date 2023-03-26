2228e5cd0c80247c778f0e1dd27e5a95cd23ea8e
abux-hzn18 <mdyahasan18@gmail.com>
2023-03-26 13:31:32 +0530

    Copied Obuli's file to my project

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# LOGIN-PAGE
-
-TO VIEW THE DISPLAY PAGE CLICK THE LINK : https://abux-hzn18.github.io/LOGIN-PAGE/index.html
+# SampleProject
+This project is for sample 
---
+++ src/main/java/testartifact/TestClass.java
@@ -0,0 +1,21 @@
+/**
+ * @author Obuli
+ */
+package testartifact;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I am doing training on Jenkins........");
+		System.out.println("This is feature branch validation by obuli.");
+		
+	}
+	
+	public int demo(int a,int b) {
+		
+		int c= a+b;
+		return c;
+	}
+
+}
